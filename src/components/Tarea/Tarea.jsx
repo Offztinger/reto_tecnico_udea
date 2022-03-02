18a4c6ba1a55517e8d22c4ae768c60e60192cdb5
Luis Alejandro Calvo Alvarez <Offztinger@gmail.com>
2022-03-01 23:02:19 -0500

    Frontend finalizado

--- src/components/Tarea/Tarea.jsx
+++ src/components/Tarea/Tarea.jsx
@@ -2,12 +2,43 @@ import React from "react";
 import "./Tarea.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBackward } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 export default function Tarea() {
   return (
     <div className="newTareaContainer">
       <div className="navbar">
-        <FontAwesomeIcon icon={faBackward} />
+        <div className="contenedorBack">
+          <p className="subtitleBack">Volver atras...</p>
+          <Link to="/">
+            <FontAwesomeIcon className="fontAwesome" icon={faBackward} />
+          </Link>
+        </div>
+      </div>
+      <div className="createContainer">
+        <div className="tarea-container">
+          <div className="tarea-contenido">
+            <div className="detalles-tarea">
+              <p className="tarea-titulo">
+                <input
+                  type="text"
+                  name="titulo"
+                  id="titulo"
+                  placeholder="Digite el titulo de la tarea"
+                />
+              </p>
+              <p className="tarea-fecha">
+                <input type="date" name="fecha" id="fecha" />
+              </p>
+            </div>
+            <div className="categoria-tarea">
+              <p className="tarea-titulo-categoria">Categoria:</p>
+              <p className="tarea-categoria">
+                <input type="text" name="text" id="categoria" />
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
